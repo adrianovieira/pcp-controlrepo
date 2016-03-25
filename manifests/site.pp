f8743575eb3fb29e6938050a68a7276df687d335
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-03-25 20:05:45 -0300

    Adicionada classificacao para node puppet-pcpm

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,14 @@
 node 'puppetmq.hacklab' {
-
   include role::broker
-
 }
 node 'puppetdb.hacklab' {
-
   include role::puppetdb
-
 }
 node 'puppetserver.hacklab' {
-
   include role::puppetmaster
-
+}
+node 'puppet-pcpm.hacklab' {
+  include role::puppetmaster
+  include role::puppetdb
+  include role::broker
 }

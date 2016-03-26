a46d2ad1217984123601f51f727f2966d864a776
Guto Carvalho <gutocarvalho@instruct.com.br>
2016-03-26 07:35:00 -0300

    Corrigindo ordem das classes para o puppetmaster, hiera deve estar depois de server

--- site/role/manifests/puppetmaster.pp
+++ site/role/manifests/puppetmaster.pp
@@ -1,6 +1,6 @@
 class role::puppetmaster {
   include profile::ntp
-  include profile::puppet::hiera
   include profile::puppet::server
+  include profile::puppet::hiera
   include profile::mcollective::server
 }

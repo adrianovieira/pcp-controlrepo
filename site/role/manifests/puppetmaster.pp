74284b3b9433c378827932658abc3b9e7bf04a62
Guto Carvalho <gutocarvalho@gmail.com>
2016-04-30 08:15:58 -0300

    Ordering class exec

--- site/role/manifests/puppetmaster.pp
+++ site/role/manifests/puppetmaster.pp
@@ -4,4 +4,11 @@ class role::puppetmaster {
   include profile::puppet::hiera
   include profile::puppet::r10k
   include profile::mcollective::server
+  
+  Class['profile::ntp'] -> 
+  Class['profile::puppet::server'] -> 
+  Class['profile::puppet::hiera'] ->
+  Class['profile::puppet::r10k'] ->
+  Class['profile::mcollective::server']
+  
 }

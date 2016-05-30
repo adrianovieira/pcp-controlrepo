79c23750bfe84bdb5f691d278dda196234a5b4b4
Adriano Vieira <adriano.vieira@dataprev.gov.br>
2016-05-30 18:35:02 -0300

    Add serveraliases for puppetexplorer.hacklab

--- site/profile/manifests/puppetdb/frontend.pp
+++ site/profile/manifests/puppetdb/frontend.pp
@@ -7,6 +7,7 @@ class profile::puppetdb::frontend  {
   class { 'puppetexplorer':
     package_ensure => $puppetexplorer_version,
     vhost_options  => {
+      serveraliases => ["puppetexplorer.${::domain}"],
          'priority' => '05',
           rewrites  => [ { rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] } ]
     },
